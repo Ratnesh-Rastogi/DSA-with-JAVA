8214a8c4bc8b43a3d4da63f8abcadd7281651bca
Ratnesh Rastogi <ratneshrastogi00@gmail.com>
2026-02-01 22:54:14 +0530

    I have uploaded Basics, Conditionals and loops in Java

---
+++ loops.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class loops {
+        public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+
+        //Loops
+        // //while loop
+
+        // int count = 1;
+        // while(count != 6){
+        //     System.out.println(count);
+        //     count++;
+        // }
+
+        // //For loop
+        // int count;
+        // for(count=1;count!=6;count++){
+        //     System.out.println(count);
+        // }
+    }
+}
