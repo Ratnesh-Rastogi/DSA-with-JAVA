d81250e2f1fad3b5e1afa48bb4c2d8f60f27617d
Ratnesh Rastogi <ratneshrastogi00@gmail.com>
2026-02-03 00:10:59 +0530

    Added swich case problems

--- loops.java
+++ loops.java
@@ -4,69 +4,69 @@ public class loops {
         public static void main(String[] args){
             Scanner input = new Scanner(System.in);
 
-        // Loops
-        //while loop
+        // // Loops
+        // //while loop
 
-        int count = 1;
-        while(count != 6){
-            System.out.println(count);
-            count++;
-        }
+        // int count = 1;
+        // while(count != 6){
+        //     System.out.println(count);
+        //     count++;
+        // }
 
-        //For loop
-        int count;
-        for(count=1;count!=6;count++){
-            System.out.println(count);
-        }
+        // //For loop
+        // int count;
+        // for(count=1;count!=6;count++){
+        //     System.out.println(count);
+        // }
 
-        // nth Fibonnacci
+        // // nth Fibonnacci
 
-        System.out.print("Enter the Value:- ");
-        int n = input.nextInt();
-        int a = 0;
-        int b = 1;
-        int count = 2;
+        // System.out.print("Enter the Value:- ");
+        // int n = input.nextInt();
+        // int a = 0;
+        // int b = 1;
+        // int count = 2;
 
-        while(count<=n){
-            int temp = b;
-            b = b + a;
-            a = temp;
-            count++;
-        }
-        System.out.println(b);
+        // while(count<=n){
+        //     int temp = b;
+        //     b = b + a;
+        //     a = temp;
+        //     count++;
+        // }
+        // System.out.println(b);
 
 
-        //Count the Number of Occurence of a number
-        System.out.print("Enter the Value:- ");
-        int n = input.nextInt();
-        System.out.print("Enter the number to be searched:- ");
-        int k = input.nextInt();
-        int count = 0;
-        while(n>0){
-            int rem = n%10;
-            if(k == rem){
-                count++;
-            }
-            n = n/10;
-        }
-        System.out.println(count);
+        // //Count the Number of Occurence of a number
+        // System.out.print("Enter the Value:- ");
+        // int n = input.nextInt();
+        // System.out.print("Enter the number to be searched:- ");
+        // int k = input.nextInt();
+        // int count = 0;
+        // while(n>0){
+        //     int rem = n%10;
+        //     if(k == rem){
+        //         count++;
+        //     }
+        //     n = n/10;
+        // }
+        // System.out.println(count);
         
 
-        //Find the reverse of given number
-        System.out.print("Enter the number:- ");
-        int num = input.nextInt();
-        int rev = 0;
-        while(num>0){
-            int rem = num % 10;
-            rev = rev * 10 + rem;
-            num = num/10;
-        }
-        System.out.println(rev);
+        // //Find the reverse of given number
+        // System.out.print("Enter the number:- ");
+        // int num = input.nextInt();
+        // int rev = 0;
+        // while(num>0){
+        //     int rem = num % 10;
+        //     rev = rev * 10 + rem;
+        //     num = num/10;
+        // }
+        // System.out.println(rev);
 
 
 
-        //Caluculator
-        System.out.print("enter the operator:- ");
+        //Caluculator using Switch Case
+        System.out.print("enter the operator :- ");
         char op = input.next().trim().charAt(0);
         
         System.out.print("Enter the 1st number:- ");
@@ -74,28 +74,32 @@ public class loops {
         System.out.print("enter the 2nd number:- ");
         int n2 = input.nextInt();
 
-        if(op == '+' ||op == '*' ||op == '/'||op == '%'||op == '-'){
-            if (op == '+'){
-                int n = n1 + n2;
-                System.out.println(n);
-            }
-            else if(op == '-'){
-                int n = n1 - n2;
-                System.out.println(n);
-            }
-            else if(op == '*'){
-                int n = n1 * n2;
-                System.out.println(n);
-            }
-            else if(op == '/'){
-                int n = n1 / n2;
-                System.out.println(n);
-            }
-            else{
-                int n = n1 % n2;
-                System.out.println(n);
-            }
+        switch(op){
+            case '+':
+                System.out.println(n1+" + "+n2 + " = " + (n1 + n2));
+                break; 
+
+            case '-':
+                System.out.println(n1+" - " +n2+ " = "+(n1 - n2));
+                break;
+
+            case '*':
+                System.out.println(n1+" * "+n2+" = "+(n1 * n2));
+                break;
+
+            case '/':
+                System.out.println(n1+" / "+n2+" = "+(n1 / n2));
+                break;
+
+            case '%':
+                System.out.println((n1+" % "+n2+" = "+n1 % n2));
+                break;
+
+            default:
+                System.out.println("Invalid Operator");
+                break;
         }
+
 
 
     }
